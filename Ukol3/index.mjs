23b0f2c7268ea4ba407a5e0898a88f0fe090c9e3
Mikulas <mikulasbilek7@gmail.com>
2026-03-05 19:51:43 +0100

    zakladni verze ukolu3

---
+++ Ukol3/index.mjs
@@ -0,0 +1,32 @@
+import fs from 'fs/promises'
+
+
+
+async function read(){
+    try{
+        const numberOfFiles = await fs.readFile('instrukce.txt');
+        return numberOfFiles;
+
+    }catch(err){
+        console.error('Jejda');
+    }
+}
+
+async function createFiles(){
+    const numberOfFiles = await read();
+    for(let i = 0; i < numberOfFiles; i++){
+        try{
+            let fileName = i+'.txt';
+            let fileText = 'Soubor '+ i;
+            fs.writeFile(fileName,fileText);
+            if(i = numberOfFiles){
+                console.log("Soubory vytvořeny")
+            }
+
+        }catch(err){
+            console.error('Jejda');
+        }
+    }
+
+}
+createFiles();
