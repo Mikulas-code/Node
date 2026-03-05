fa3d2430cc0fe4cdb3631436d04b64ae3178f4a6
Mikulas <mikulasbilek7@gmail.com>
2026-03-05 16:06:41 +0100

    Dokončen ukol2

--- Ukol2/index.mjs
+++ Ukol2/index.mjs
@@ -1,13 +1,44 @@
 import fs from 'fs'
 
-function readFile () {
-   fs.readFile('vstup.txt', (err,data)=>{
-    if(err){
-        console.error('chyba')
-    }else{
-        console.log(data.toString());
+function readFile (input) {
+
+    return new Promise((resolve,reject)=>{
+        fs.readFile(input, (err,data)=>{
+            if(err){
+                reject(err);
+            }else{
+                resolve(data);
+            }
+        })
     }
 
-   })
+    );
+   
 }
-readFile();
+
+function writeFile (path, data) {
+	return new Promise((resolve, reject) => {
+		fs.writeFile(path, data, (err) => {
+			if (err) {
+				reject(err)
+			} else {
+				resolve(data)
+			}
+		})
+	})
+}
+
+async function copyWrite() {
+    const instrukce = await readFile('instrukce.txt');
+    const instrukceText = instrukce.toString();
+    const vstup = instrukceText.split(',')[0];
+    const vystup = instrukceText.split(',')[1];
+    console.log(vstup);
+    console.log(vystup);
+
+    const obsah = await readFile(vstup);
+
+    writeFile(vystup,obsah);
+
+}
+copyWrite();
